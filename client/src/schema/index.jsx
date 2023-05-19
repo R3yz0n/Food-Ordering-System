94011c81398334dac8b3565bcc4dea67494527b1
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-20 01:30:19 +0545

    login/signup recepice schema

---
+++ client/src/schema/index.jsx
@@ -0,0 +1,49 @@
+import * as Yup from 'yup';
+
+
+export const registrationSchema = Yup.object({
+
+    userName: Yup.string()
+        .matches(/^[a-zA-Z]+$/, 'Username can only contain alphabetical characters.')
+        .min(5, 'Username must be at least 5 characters long.')
+        .max(15, 'Username cannot be longer than 15 characters.')
+        .required('Username is required.'),
+
+    email: Yup.string()
+        .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Email must have \'@\' followed by \'.com\'.')
+        .required('Email is required.'),
+
+    password: Yup.string()
+        .min(6, 'Password must be at least 6 characters long.')
+        .matches(/^(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])[0-9a-zA-Z!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]{8,}$/,
+            'Password must contain at least one number and one special character.')
+        .required('Password is required.'),
+
+    confirmPassword: Yup.string().required('Confirm password is required.').oneOf([Yup.ref('password'), null], "Password must match.")
+
+
+})
+
+
+export const loginSchmea = Yup.object({
+
+    email: Yup.string()
+        .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Email must have \'@\' followed by \'.com\'.')
+        .required('Email is required.'),
+
+
+
+    password: Yup.string()
+        .min(6, 'Password must be at least 6 characters long.')
+        .matches(/^(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])[0-9a-zA-Z!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]{8,}$/,
+            'Password must contain at least one number and one special character.')
+        .required('Password is required.'),
+
+
+
+})
+
+
+
+
+
