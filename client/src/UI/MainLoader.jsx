aece21590d932e224edc50ff790c1887e58dc1a9
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-26 20:54:06 +0545

    fixed css

--- client/src/UI/MainLoader.jsx
+++ client/src/UI/MainLoader.jsx
@@ -5,12 +5,12 @@ import { RotatingLines } from 'react-loader-spinner'
 
 const MainLoader = () => {
     return (
-        <motion.div className='opacity-50 fixed z-50 inset-0 backdrop-blur-sm flex items-center justify-center w-full backdrop-filter' {...fadeInOut}>
+        <motion.div className='opacity-50 fixed z-50 inset-0 backdrop-blur-sm flex items-center justify-center w-full backdrop-filter ' {...fadeInOut}>
             <div className='flex items-center justify-center container'>
 
                 <RotatingLines
                     strokeColor="black"
-                    strokeWidth="5"
+                    strokeWidth="3"
                     animationDuration="1.1"
                     width="100"
                     visible={true}
