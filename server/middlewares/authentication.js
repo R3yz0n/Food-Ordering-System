30804d68255c38779aea08548f3f5d90f70512c4
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-26 09:51:13 +0545

    middleware for auth and role

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const env = require('dotenv')
+env.config();
+
+
+
+const checkAuthentication = async (req, res, next) => {
+    console.log(req.headers.authorization);
+    console.log(req.headers.authorization.split(" ")[1]);
+    if (!req.headers.authorization)
+        return res.status(403).json({ message: "Unauthorized user." })
+
+
+
+
+    try {
+        // const decodedUser = jwt.verify(req.headers.authorization, 'secret')
+        console.log('-------');
+        const decodedUser = jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_SECRET)
+        console.log(decodedUser);
+        console.log('next auth');
+        next()
+
+
+    }
+    catch (error) {
+        console.log(error.message);
+
+        return res.status(401).json({
+            message: "Unauthorized user."
+        })
+
+    }
+
+
+
+
+}
+
+module.exports = { checkAuthentication: checkAuthentication }
+
---
+++ server/middlewares/role.js
@@ -0,0 +1,59 @@
+const env = require('dotenv')
+env.config();
+const jwt = require('jsonwebtoken')
+const { user } = require('../models')
+const checkRole = async (req, res, next) => {
+
+
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
+        const decodedUser = jwt.verify(token, process.env.JWT_SECRET);
+        // console.log(decodedUser);
+        // console.log('role');
+        // console.log(decodedUser.role);
+
+
+        try {
+            const userData = await user.findByPk(decodedUser.id)
+            // console.log(userData);
+
+            if (userData.dataValues.role === decodedUser.role) {
+
+                console.log('authorized');
+                next()
+            }
+            else {
+                console.log('aunthorized');
+                res.status(401).json({
+                    message: "Unauthorized user."
+                })
+            }
+
+        }
+        catch (error) {
+            res.status(500).json({
+                message: "Something went wrong."
+            })
+
+        }
+
+
+
+    }
+    catch (error) {
+        console.log(error);
+
+        res.status(401).json({
+            message: "Invalid token."
+        });
+
+    }
+
+
+}
+
+
+module.exports = {
+    checkRole: checkRole
+}
