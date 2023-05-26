606acdab53b55ec72e5ea8e5b5e58fd2f59b9df1
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-26 20:55:12 +0545

    fetched food items

--- client/src/components/dashboard/Items.jsx
+++ client/src/components/dashboard/Items.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AiFillDelete } from 'react-icons/ai'
 import { AiFillEdit } from 'react-icons/ai'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllItems } from '../../store/product/productAction';
+import { APIURL } from '../../APIURL';
+import { MainLoader } from '../../UI';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { clearFields } from '../../store/product/productSlice';
+
 
 const Items = () => {
+    const dispatch = useDispatch()
+    const { items, success, loading } = useSelector(state => state.product)
+    // console.log(items[0]);
 
 
-    const data = [
-        { id: 1, name: 'Item 1', category: 'Category 1', price: 10.99, image: 'image1.jpg' },
-        { id: 2, name: 'Item 2', category: 'Category 2', price: 19.99, image: 'image2.jpg' },
-        { id: 3, name: 'Item 3', category: 'Category 1', price: 14.99, image: 'image3.jpg' },
-        { id: 4, name: 'Item 4', category: 'Category 3', price: 9.99, image: 'image4.jpg' },
-        { id: 5, name: 'Item 5', category: 'Category 2', price: 12.99, image: 'image5.jpg' },
-        { id: 1, name: 'Item 1', category: 'Category 1', price: 10.99, image: 'image1.jpg' },
-        { id: 1, name: 'Item 1', category: 'Category 1', price: 10.99, image: 'image1.jpg' },
-        { id: 2, name: 'Item 2', category: 'Category 2', price: 19.99, image: 'image2.jpg' },
-        { id: 3, name: 'Item 3', category: 'Category 1', price: 14.99, image: 'image3.jpg' },
-        { id: 4, name: 'Item 4', category: 'Category 3', price: 9.99, image: 'image4.jpg' },
-        { id: 5, name: 'Item 5', category: 'Category 2', price: 12.99, image: 'image5.jpg' },
-        { id: 1, name: 'Item 1', category: 'Category 1', price: 10.99, image: 'image1.jpg' },
 
 
-    ];
+
+    useEffect(() => {
+        dispatch(getAllItems())
+
+    }, [dispatch])
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            dispatch(clearFields())
+
+        }, 1000);
+
+    }, [success, dispatch])
+
 
     return (
         <section className='py-10'>
+            {loading && <MainLoader />}
 
             <table className="w-full h-full">
-                <thead className="bg-white">e
+                <thead className="">
                     <tr>
                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Edit/Delete</th>
                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
@@ -35,19 +47,21 @@ const Items = () => {
                         <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Image</th>
                     </tr>
                 </thead>
-                <tbody className="bg-purple-50 overflow-y-scroll">
-                    {data.map(item => (
-                        <tr key={item.id}>
-                            <td className="px-6 py-4 whitespace-nowrap flex gap-3">
+                <tbody className=" overflow-y-scroll">
+                    {items?.map(item => (
+                        <tr key={item.id} className='border-2'>
+
+                            <td className="px-6 py-4 whitespace-nowrap flex gap-3 h-20">
                                 <AiFillDelete className='text-2xl' />
                                 <AiFillEdit className='text-2xl' />
 
                             </td>
-                            <td className="px-6 py-4 whitespace-nowrap"><img src={item.image} alt={item.name} className="h-8 w-8" /></td>
+                            <td className="w-14 h-14"><LazyLoadImage src={`${APIURL}/file/${item.image}`} alt='loading' className="h-full w-full rounded-full " delayTime={900} effect='opacity' /></td>
 
                             <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
                             <td className="px-6 py-4 whitespace-nowrap">{item.category}</td>
                             <td className="px-6 py-4 whitespace-nowrap">{item.price}</td>
+
                         </tr>
                     ))}
                 </tbody>
@@ -65,3 +79,5 @@ const Items = () => {
 }
 
 export default Items
+
+
