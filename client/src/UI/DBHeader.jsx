03fcb84a2edaa2c96b838362b613b0095aba7b1d
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-27 22:44:16 +0545

    fixed css

--- client/src/UI/DBHeader.jsx
+++ client/src/UI/DBHeader.jsx
@@ -12,10 +12,10 @@ const DBHeader = () => {
     const { userData } = auth
     const dispatch = useDispatch()
     return (
-        <section className='w-full flex items-center justify-between gap-3 border-[1px] px-3'>
+        <section className='w-full flex items-center justify-between gap-3  px-3 '>
 
             {/* left */}
-            <p className='text-2xl text-headingColor' >Welcome to FoodZone
+            <p className='text-2xl text-headingColor font-semibold  font-sans ' >Welcome to FoodZone
                 {auth?.userData && <span className='block text-base text-black'>Hello {auth?.userData?.userName}.</span>}
             </p>
 
@@ -25,15 +25,15 @@ const DBHeader = () => {
 
                 <div className='flex items-center justify-center gap-4'>
 
-                    <div className='flex items-center justify-center gap-3 px-4 py-2 backdrop-blur-md rounded-md shadow-md '>
-                        <MdSearch className='text-gray-700 text-2xl' />
-                        <input type="text" placeholder='Search here..' className=' bg-transparent w-32 outline-none text-base' />
+                    <div className='flex items-center justify-center gap-3 px-4 py-2 backdrop-blur-md rounded-md shadow-lg  bg-gray-100'>
+                        <MdSearch className='text-gray-700 text-2xl ' />
+                        <input type="text" placeholder='Search here..' className=' bg-transparent w-48 outline-none text-base' />
                         <BsToggles2 className='text-gray-700 text-2xl' />
                     </div>
 
 
-                    <motion.div {...btnClick} className='w-10 rounded-md cursor-pointer backdrop-blur-md shadow-md flex items-center justify-center h-10 gap-2' >
-                        <BsFillBellFill className='text-gray-400 text-xl h-full' />
+                    <motion.div {...btnClick} className='w-10 bg-gray-100 rounded-md cursor-pointer backdrop-blur-md shadow-md flex items-center justify-center h-10 gap-2 ' >
+                        <BsFillBellFill className='text-gray-500 text-xl h-full' />
                     </motion.div>
 
 
@@ -45,7 +45,7 @@ const DBHeader = () => {
                                 whileHover={{ scale: 1.1 }} referrerPolicy='no-referrers' />
                         </div>
 
-                        <motion.div {...btnClick} className='w-10 rounded-md cursor-pointer backdrop-blur-md shadow-md flex items-center justify-center h-10 ' >
+                        <motion.div {...btnClick} className='w-10 rounded-md cursor-pointer backdrop-blur-md shadow-md flex items-center justify-center h-10  bg-gray-100' >
                             <MdLogout className='text-2xl text-textColor group-hover::text-headingColor h-10' onClick={() => dispatch(logout())} />
                         </motion.div>
 
