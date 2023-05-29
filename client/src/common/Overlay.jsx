189ee48f9f3960fb8cb0f082c22c4c7d10bc970c
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-29 15:50:49 +0545

    custom backdrop overlay

---
+++ client/src/common/Overlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Overlay = ({ children }) => (
+    <>
+        <div className="fixed inset-0 flex items-center justify-center z-30 bg-black opacity-75 w-screen h-screen b backdrop-blur-[1px]"></div>
+
+        {children}
+    </>
+
+);
+export default Overlay;
