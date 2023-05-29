67cb294eab1eb2fbfd1f839a73a98657fe7dcba4
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-29 15:49:53 +0545

    removed strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -12,19 +12,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AnimatePresence>
-        <Provider store={store}>
+  <BrowserRouter>
+    <AnimatePresence>
+      <Provider store={store}>
 
-          <App />
-        </Provider>
+        <App />
+      </Provider>
 
 
-      </AnimatePresence>
-    </BrowserRouter>
+    </AnimatePresence>
+  </BrowserRouter>
 
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
