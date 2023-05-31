6e98d0657a05746547da13cc0e7c8f967c4609c5
Reyzon <reyzon.thapa2020@gmail.com>
2023-05-31 23:58:39 +0545

    added landng home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Landing/Login'
 import Register from './components/Landing/Register'
 import Main from './components/Main'
 import AdminRoute from './helpers/AdminRoute'
+import Aboutus from './components/Landing/Aboutus'
 
 
 const App = () => {
@@ -14,11 +15,13 @@ const App = () => {
 
   return (
 
-    <main className='w-screen min-h-screen h-auto flex flex-col items-center justify-center'>
+    <main className='w-screen min-h-screen h-auto flex flex-col items-center justify-center '>
 
       <Routes>
         <Route path='/*' element={<Main />} />
         <Route path='/login' element={<Login />} />
+        {/* <Route path='/login' element={<Aboutus />} /> */}
+
         <Route path='/register' element={<Register />} />
         <Route path='/dashboard/*' element={<AdminRoute>
           <Dashboard />
