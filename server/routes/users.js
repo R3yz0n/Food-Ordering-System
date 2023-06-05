e67fd5bc442f9db6312aac6823ed0e382a3a28e5
Reyzon <reyzon.thapa2020@gmail.com>
2023-06-05 11:39:27 +0545

    user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const { checkAuthentication } = require('../middlewares/authentication')
-
-const router = express.Router();
 const { checkRole } = require('../middlewares/role')
 const userController = require('../controllers/user.controller')
 
+const router = express.Router();
+
 router.get('/', checkAuthentication, checkRole, userController.getAllUsers)
+router.get('/:id', checkAuthentication, userController.getUser)
 
 module.exports = router;
