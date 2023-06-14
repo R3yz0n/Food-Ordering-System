ab3dbdfd11a8584e94a393af41d4cfa1239abe68
Reyzon <reyzon.thapa2020@gmail.com>
2023-06-15 01:57:59 +0545

    advance search route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,16 +1,31 @@
 const express = require('express')
 const { user } = require('../models')
 const models = require('../models')
+const { Op } = require('sequelize');
 
 
 
 const getAllUsers = async (req, res) => {
 
     console.log('got here');
+    console.log(req.query);
+    const { userName } = req.query;
+    const query = userName
+        ? {
+            where: {
+                userName: { [Op.startsWith]: userName },
+            },
+        }
+        : {};
 
     try {
-        const allUsers = await user.findAll(); // Assuming you are using Mongoose for your models
-        console.log(allUsers);
+        if (req.query.userName === 'all') {
+            const allUsers = await user.findAll();
+
+            return res.json(allUsers);
+
+        }
+        const allUsers = await user.findAll(query);
 
         res.json(allUsers);
     } catch (error) {
@@ -42,6 +57,15 @@ const getUser = async (req, res) => {
         res.status(500).json({ message: 'Something went wrong' });
     }
 };
+
+
+
+
+
+
+
+
+
 
 module.exports = {
     getAllUsers: getAllUsers,
