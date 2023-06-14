e598b83c2f69ffc72677749e0f2d2fef6dc8767f
Reyzon <reyzon.thapa2020@gmail.com>
2023-06-15 01:55:17 +0545

    added onchange to search bar and clear when category changes

--- client/src/components/menu/MenuCat.jsx
+++ client/src/components/menu/MenuCat.jsx
@@ -6,7 +6,7 @@ import { BsSearch } from 'react-icons/bs'
 import SwiperScroll from './SwiperScroll';
 
 
-const MenuCat = ({ handleFilter, category, handleSearch, searchKeyword }) => {
+const MenuCat = ({ handleFilter, category, handleSearch, searchValue }) => {
 
 
 
@@ -24,7 +24,7 @@ const MenuCat = ({ handleFilter, category, handleSearch, searchKeyword }) => {
                     <div className="mb-3">
 
                         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-                            <input type="search" className="relative focus:shadow-md shadow-black m-0 -mr-0.5 block w-[1px]  min-w-0 flex-auto rounded-l border-2 border-solid border-gray-600 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-lg text-black font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:border-red-600  focus:text-neutral-700  focus:outline-none" placeholder="Search food here" onChange={(e) => handleSearch(e.target.value)} value={searchKeyword} />
+                            <input type="search" className="relative focus:shadow-md shadow-black m-0 -mr-0.5 block w-[1px]  min-w-0 flex-auto rounded-l border-2 border-solid border-gray-600 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-lg text-black font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:border-red-600  focus:text-neutral-700  focus:outline-none" placeholder="Search food here" onChange={(e) => handleSearch(e.target.value)} value={searchValue} />
 
                             <motion.button {...btnClick} className="relative z-[2] flex items-center rounded-r bg-gray-900 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md  hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg" type="button" >
                                 <BsSearch className='text-xl' />
