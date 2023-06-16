60a36883a532f5a24224ff61188bcf06aa779271
Reyzon <reyzon.thapa2020@gmail.com>
2023-06-16 23:16:37 +0545

    cart slide right animation

--- client/src/animations/index.jsx
+++ client/src/animations/index.jsx
@@ -42,6 +42,12 @@ export const straggerFadeInOut = (i) => {
     }
 }
 
+export const slideLeft = {
+    initial: { opacity: 0, x: 30, },
+    animate: { opacity: 1, x: 0, },
+    exit: { opacity: 0, x: 30, }
+}
+
 export const error = {
     initial: { opacity: 0 }, animate: { opacity: 1 }, exit: { opacity: 0 }, transition: { duration: 1 }
 
