9334a9732e1de9c01ce6961494aaa62d7b9fd1cf
Reyzon <reyzon.thapa2020@gmail.com>
2023-06-24 22:32:20 +0545

    added add to cart functionality

--- client/src/components/cart/CartItem.jsx
+++ client/src/components/cart/CartItem.jsx
@@ -1,17 +1,66 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 import { HiCurrencyRupee } from 'react-icons/hi';
-import { AiFillMinusCircle } from 'react-icons/ai'
-import { AiFillPlusCircle } from 'react-icons/ai'
-import { btnClick } from '../../animations';
+import { btnClick, fade, pop } from '../../animations';
+import { APIURL } from '../../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrementQuantity, getAllCartItems, incrementQuantity } from '../../store/cart/cartAction';
+import { clearFields } from '../../store/cart/cartSlice';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 
 const CartItem = ({ item }) => {
-  console.log(item);
+  const dispatch = useDispatch()
+  const { userData } = useSelector(state => state.currUser)
+  // console.log(item.quantity);
+
+  const handleIncrement = async () => {
+
+    const userId = userData.id
+    const itemId = item.id
+
+
+    try {
+      const isIncreased = await dispatch(incrementQuantity({ userId, itemId })).unwrap()
+      const fetchUpdatedCart = await dispatch(getAllCartItems(userData?.id)).unwrap()
+      dispatch(clearFields())
+
+    } catch (error) {
+
+      console.log('error');
+    }
+
+
+
+
+  }
+
+  const handleDecrement = async () => {
+    const userId = userData.id
+    const itemId = item.id
+
+
+    try {
+      const isIncreased = await dispatch(decrementQuantity({ userId, itemId })).unwrap()
+      const fetchUpdatedCart = await dispatch(getAllCartItems(userData?.id)).unwrap()
+      dispatch(clearFields())
+
+    } catch (error) {
+
+      console.log('error');
+    }
+
+
+
+
+  }
+
+
   return (
     <motion.section className='w-full flex items-center justify-start bg-zinc-800 rounded-md drop-shadow-md px-2 sm:pl-2 xs:pr-3 sm:pr-4 gap-4'>
 
       <div className='h-20  w-20 sm:w-28 sm:h-24 py-2 flex'>
-        <img src={item.image} className='w-full h-full object-contain m-auto' alt="Cart Item" />
+        <LazyLoadImage src={`${APIURL}/file/${item.image}`} className='w-full h-full object-contain m-auto' alt="Cart Item" />
       </div>
 
       <div className='flex items-center justify-start gap-1 w-full h-20 '>
@@ -27,17 +76,17 @@ const CartItem = ({ item }) => {
 
         <p className='text-sm font-semibold text-red-400 ml-auto flex items-center justify-center gap-1'>
           <HiCurrencyRupee className='text-red-500 text-lg' />
-          1200
+          {item.price * item.quantity}
         </p>
 
         {/* buttons */}
         <div className='ml-auto flex items-center justify-center gap-3'>
-          <motion.div className='h-7 w-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md drop-shadow-md bg-zinc-950 cursor-pointer active:bg-green-600' {...btnClick}>
-            <p className='text-xl sm:text-xl font-semibold text-primary '>+</p>
+          <motion.div className='h-7 w-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md drop-shadow-md bg-zinc-950 cursor-pointer active:bg-green-600' {...btnClick} onClick={handleIncrement}>
+            <p className='text-xl sm:text-xl font-semibold text-primary ' >+</p>
           </motion.div>
-          <p className='text-base sm:text-lg text-primary font-semibold'>{2}</p>
-          <motion.div className='w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md drop-shadow-md bg-zinc-950 cursor-pointer active:bg-blue-600' {...btnClick}>
-            <p className='text-xl sm:text-xl font-semibold text-primary'>--</p>
+          <p className='text-base sm:text-lg text-primary font-semibold flex'>x{item.quantity}</p>
+          <motion.div className='w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md drop-shadow-md bg-zinc-950 cursor-pointer active:bg-blue-600' {...btnClick} onClick={handleDecrement}>
+            <p className='text-xl sm:text-xl font-semibold text-primary' >--</p>
           </motion.div>
 
         </div>
