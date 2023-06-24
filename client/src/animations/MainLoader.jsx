088557003c7b2dd3dfe4031e9d3192b4b68c0f12
Reyzon <reyzon.thapa2020@gmail.com>
2023-06-24 22:31:22 +0545

    fixed backdrop

--- client/src/animations/MainLoader.jsx
+++ client/src/animations/MainLoader.jsx
@@ -5,7 +5,7 @@ import { HashLoader } from 'react-spinners'
 
 const MainLoader = () => {
     return (
-        <motion.div className='opacity-50 fixed z-50 inset-0 backdrop-blur-[2px] flex items-center justify-center w-screen h-screen backdrop-filter ' {...fadeInOut}>
+        <motion.div className='opacity-50 fixed  z-50 inset-0 backdrop-blur-[2px] flex items-center justify-center w-screen min-h-screen backdrop-filter ' {...fadeInOut}>
             <div className='flex items-center justify-center container'>
 
                 <HashLoader color="#d63691" size={85} />
