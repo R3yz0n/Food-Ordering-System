f41a578ce0ce55edc139ced4efaa22131ece2bfd
Reyzon <reyzon.thapa2020@gmail.com>
2023-07-01 20:54:23 +0545

    order models

---
+++ server/models/orders.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class orders extends Model {
+
+    static associate(models) {
+      orders.belongsTo(models.users);
+
+      orders.belongsToMany(models.items, { through: models.orderItems, onDelete: 'CASCADE' });
+
+    }
+  }
+  orders.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+
+      }
+    },
+    totalAmount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+
+    },
+
+    description: DataTypes.STRING,
+    status: {
+      type: DataTypes.ENUM('Preparing', 'Cancelled', 'Delivered'),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'orders',
+  });
+  return orders;
+};
