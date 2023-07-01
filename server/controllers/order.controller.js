6f3e3f4abca66e382a916022281baefa68ea98d1
Reyzon <reyzon.thapa2020@gmail.com>
2023-07-01 20:53:39 +0545

    created boilterplate

---
+++ server/controllers/order.controller.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const models = require('../models')
+const { Op } = require('sequelize');
+
+
+const createOrder = async (req, res) => {
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    createOrder: createOrder
+
+}
