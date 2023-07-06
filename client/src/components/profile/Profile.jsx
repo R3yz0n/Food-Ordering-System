b3b3642ca026bac37441c207d0392ab7acfa8590
Reyzon <reyzon.thapa2020@gmail.com>
2023-07-06 20:56:05 +0545

    profile updater

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,68 +1,25 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import Avatar from "../../assests/Avatar.png";
+import Form from "./Form";
 
 const Profile = () => {
-    const { userData } = useSelector(state => state.currUser)
+  const { userData } = useSelector((state) => state.currUser);
 
-    return (
-        <main className='pt-24  w-full h-screen  flex items-center justify-center'>
+  return (
+    <main className=" pt-28 sm:pt-40  w-full h-auto sm:h-screen pb-8 bg-gray-100 px-8 md:px-0 b">
+      <section className=" flex flex-col gap-3 sm:flex-row justify-center sm:gap-20  mx-auto py-10 px-5 bg-[rgb(235,240,248)] md:w-[750px] lg:w-[800px] rounded-lg  profilecard">
+        <div className="h-[95%] flex">
+          <img src={Avatar} alt="Avatar" className="w-40 h-40 mx-auto" />
 
-            <div className='h-[95%] w-4/5 '>
+          {/* <button className=" bg-red-500 px-5 py-1 text-white rounded-md mt-5 flex mx-auto">
+            Edit Profile
+          </button> */}
+        </div>
+        <Form />
+      </section>
+    </main>
+  );
+};
 
-                <aside className=' w-2/3 h-full'>
-
-
-                    <div className='flex gap-10'>
-                        <aside className='flex flex-col h-fit'>
-                            <label htmlFor="">Username</label>
-                            <input className='bg-transparent outline-gray-500 border-gray-500 border-2 rounded' type="text" />
-                        </aside>
-                        <aside className='flex flex-col h-fit'>
-                            <label htmlFor="">Email</label>
-                            <input className='bg-transparent outline-gray-500 border-gray-500 border-2 rounded' type="text" />
-                        </aside>
-
-                    </div>
-
-                    <div>
-
-                        <aside className='flex flex-col h-fit'>
-                            <label htmlFor="">Phone Number</label>
-                            <input className='bg-transparent outline-gray-500 border-gray-500 border-2 rounded' type="text" />
-                        </aside>
-
-                    </div>
-
-                    <div>
-
-                        <aside className='flex flex-col h-fit'>
-                            <label htmlFor="">Address</label>
-                            <input className='bg-transparent outline-gray-500 border-gray-500 border-2 rounded' type="text" />
-                        </aside>
-
-                    </div>
-
-
-                </aside>
-
-
-
-
-
-
-
-
-
-
-            </div>
-
-
-
-
-
-
-        </main >
-    )
-}
-
-export default Profile
+export default Profile;
