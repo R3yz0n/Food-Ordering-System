e0ba448d0a9b24a5d9c3783cf57a94387e3ccb45
R3yz0n <reyzon.thapa2020@gmail.com>
2023-07-22 18:48:05 +0545

    order routes

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,13 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { checkRole } = require('../middlewares/role');
-const { createOrder } = require('../controllers/order.controller');
+const { checkRole } = require("../middlewares/role");
+const { createOrder } = require("../controllers/order.controller");
 
-router.post('/', createOrder)
-
-
-
-
+router.post("/", createOrder);
+router.get("/");
 
 module.exports = router;
