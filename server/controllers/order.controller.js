35fb715dac9bee0e99da660158c88a2cda67cb3b
R3yz0n <reyzon.thapa2020@gmail.com>
2023-08-02 23:59:34 +0545

    long time

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -5,11 +5,18 @@ const { Op } = require('sequelize');
 
 const createOrder = async (req, res) => {
     console.log();
+    // Retrieve cart items
+    const cartItems = await models.cartItem.findAll({
+        where: { cartId: 1 },
+        // include: { model: Item }, // Include the associated item
+    });
+    console.log(cartItems);
+    res.json(cartItems)
+
 
 
 
 }
-
 
 
 
--- server/routes/order.js
+++ server/routes/order.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { checkRole } = require("../middlewares/role");
 const { createOrder } = require("../controllers/order.controller");
 
-router.post("/", createOrder);
+router.post("/:id", createOrder);
 router.get("/");
 
 module.exports = router;
