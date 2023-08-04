4e6142bc7c0e3651d0a321ca9d07f9fe0a21db85
R3yz0n <reyzon.thapa2020@gmail.com>
2023-08-04 23:59:49 +0545

    s

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -8,10 +8,30 @@ const createOrder = async (req, res) => {
     // Retrieve cart items
     const cartItems = await models.cartItem.findAll({
         where: { cartId: 1 },
-        // include: { model: Item }, // Include the associated item
+        // include: { model: models. },
+        attributes: ['itemId', 'quantity']
     });
     console.log(cartItems);
-    res.json(cartItems)
+    // res.json(cartItems)
+
+
+    const itemIds=cartItems.map((item=>item.itemId))
+    const allItems=await models.items.findAll({
+        where:{id:itemIds},
+        attributes:['id', 'name','price','image','category']
+    })
+
+    console.log(allItems);
+    return res.json(allItems)
+    // const order = await models.order.create(
+    //     {
+    //         userId: 1,
+    //         totalAmount: 1
+    //     }
+    // )
+
+
+    const orderItems = await models.orderItems.create
 
 
 
