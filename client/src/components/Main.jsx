6322160da7b2c131fc41f4b4629987cb24b992ae
R3yz0n <reyzon.thapa2020@gmail.com>
2023-08-06 20:06:22 +0545

    increased the height and width for larger screen

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,65 +1,57 @@
-import React, { useEffect } from 'react'
-import Navbar from '../components/Navbar'
-import Home from '../components/home/Home'
-import { Route, Routes } from 'react-router-dom'
-import Menu from './menu/Menu'
-import Profile from './profile/Profile'
-import Contact from './contact/Contact'
-import PageNotFound from './PageNotFound'
-import Cart from './cart/Cart'
-import { useDispatch, useSelector } from 'react-redux'
-import { calculateTotalQuantity, clearFields } from '../store/cart/cartSlice'
-import { getAllCartItems } from '../store/cart/cartAction'
-import LoggedInUserRoute from '../helpers/LoggedInUserRoute'
-
+import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Home from "../components/home/Home";
+import { Route, Routes } from "react-router-dom";
+import Menu from "./menu/Menu";
+import Profile from "./profile/Profile";
+import Contact from "./contact/Contact";
+import PageNotFound from "./PageNotFound";
+import Cart from "./cart/Cart";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotalQuantity, clearFields } from "../store/cart/cartSlice";
+import { getAllCartItems } from "../store/cart/cartAction";
+import LoggedInUserRoute from "../helpers/LoggedInUserRoute";
+import Order from "./order/Order";
 
 const Main = () => {
-    const { isCartOn, cartItems } = useSelector(state => state.cart)
-    const dispatch = useDispatch()
-    const { userData } = useSelector(state => state.currUser)
+  const { isCartOn, cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.currUser);
 
-    useEffect(() => {
-        if (userData?.role && localStorage.getItem('userToken')) {
+  useEffect(() => {
+    if (userData?.role && localStorage.getItem("userToken")) {
+      dispatch(getAllCartItems(userData?.id)).then(() => {
+        dispatch(clearFields());
+      });
+    }
+  }, [userData?.id, userData?.role, dispatch]);
 
-            dispatch(getAllCartItems(userData?.id)).then(() => {
-                dispatch((clearFields()))
+  useEffect(() => {
+    dispatch(calculateTotalQuantity());
+  }, [cartItems, dispatch]);
 
-            })
+  return (
+    <main className="w-full min-h-screen  bg-primary">
+      <Navbar />
 
-        }
+      {isCartOn && <Cart />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route
+          path="/profile"
+          element={
+            <LoggedInUserRoute>
+              <Profile />
+            </LoggedInUserRoute>
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/user-orders" element={<Order />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </main>
+  );
+};
 
-    }, [userData?.id, userData?.role, dispatch]);
-
-    useEffect(() => {
-
-        dispatch(calculateTotalQuantity())
-    }, [cartItems, dispatch])
-
-    return (
-        <main className='w-full min-h-screen  bg-primary'>
-            <Navbar />
-
-            {isCartOn && <Cart />}
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/menu' element={<Menu />} />
-                <Route path='/profile' element={
-                    <LoggedInUserRoute>
-
-                        <Profile />
-                    </LoggedInUserRoute>
-                }
-                />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='*' element={<PageNotFound />} />
-
-
-
-
-            </Routes>
-
-        </main>
-    )
-}
-
-export default Main
+export default Main;
