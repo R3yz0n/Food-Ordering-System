2ff2282a7e18ac09ee47c60a3a9a39ddac84592d
R3yz0n <reyzon.thapa2020@gmail.com>
2023-08-12 17:02:23 +0545

    fix z-index for backdrop opacity

--- client/src/common/Overlay.jsx
+++ client/src/common/Overlay.jsx
@@ -1,42 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-const Overlay = ({ children, onClick }) => (
-
+const Overlay = ({ children, onClick }) => {
+  return (
     <>
-        <div className="fixed inset-0 flex items-center justify-center z-40 bg-black opacity-80  w-screen h-screen " onClick={onClick}></div>
+      <div
+        className="fixed inset-0 flex items-center justify-center z-40 bg-black opacity-80  w-screen h-screen "
+        onClick={onClick}
+      ></div>
 
-        {children}
-
-
+      {children}
     </>
-
-);
-export default Overlay
-
+  );
+};
+export default Overlay;
 
 export const CartOverlay = ({ children, onClick }) => {
-    useEffect(() => {
-        document.querySelector('html').style.overflow = 'hidden'
+  useEffect(() => {
+    document.querySelector("html").style.overflow = "hidden";
 
-        return () => {
-            document.querySelector('html').style.overflow = 'auto'
-        }
+    return () => {
+      document.querySelector("html").style.overflow = "auto";
+    };
+  }, []);
 
+  return (
+    <>
+      <div
+        className="fixed inset-0 flex items-center justify-center z-40 bg-black opacity-40  w-screen h-screen "
+        onClick={onClick}
+      ></div>
 
-
-    }, [])
-
-
-    return (
-        <>
-            <div className="fixed inset-0 flex items-center justify-center z-40 bg-black opacity-40  w-screen h-screen " onClick={onClick}></div>
-
-            {children}
-        </>
-
-    )
-}
-
-
-
-
+      {children}
+    </>
+  );
+};
