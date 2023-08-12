682c1dde1423d13f7480f3efb32c91f6fd5691aa
R3yz0n <reyzon.thapa2020@gmail.com>
2023-08-13 00:09:24 +0545

    add navbar menu overlay

--- client/src/common/Overlay.jsx
+++ client/src/common/Overlay.jsx
@@ -34,3 +34,24 @@ export const CartOverlay = ({ children, onClick }) => {
     </>
   );
 };
+
+export const NavBarOverlay = ({ children, onClick }) => {
+  useEffect(() => {
+    document.querySelector("html").style.overflow = "hidden";
+
+    return () => {
+      document.querySelector("html").style.overflow = "auto";
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        className="fixed inset-0 top-20 -left-[1px]  flex items-center justify-center z-40 bg-black opacity-70  w-screen h-screen "
+        onClick={onClick}
+      ></div>
+
+      {children}
+    </>
+  );
+};
