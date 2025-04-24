b945cfcc2c5cad03b5bedcc6d3f0dae4df9f68e7
r3yz0n <reyzon.thapa2020@gmail.com>
2025-04-25 00:54:24 +0545

    azure deployment

--- server/app.js
+++ server/app.js
@@ -1,36 +1,33 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const { sequelize } = require('./models');
+const { sequelize } = require("./models");
 
-const authRoutes = require('./routes/auth')
-const itemRoutes = require('./routes/items')
-const fileRoutes = require('./routes/files')
-const userRoutes = require('./routes/users')
-const cartRoutes = require('./routes/cart')
-const latestRoutes = require('./routes/latest')
-const orderRoutes = require('./routes/order')
-const reportRoutes = require('./routes/report')
+const authRoutes = require("./routes/auth");
+const itemRoutes = require("./routes/items");
+const fileRoutes = require("./routes/files");
+const userRoutes = require("./routes/users");
+const cartRoutes = require("./routes/cart");
+const latestRoutes = require("./routes/latest");
+const orderRoutes = require("./routes/order");
+const reportRoutes = require("./routes/report");
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
 
-
-
-app.use('/auth', authRoutes);
-app.use('/file', fileRoutes)
-app.use('/item', itemRoutes)
-app.use('/user', userRoutes)
-app.use('/cart', cartRoutes)
-app.use('/latest', latestRoutes)
-app.use('/order', orderRoutes)
-app.use('/report', reportRoutes)
-
-
-
-
+app.get("/prod", (req, res) => {
+    res.send("hello prod");
+});
+app.use("/auth", authRoutes);
+app.use("/file", fileRoutes);
+app.use("/item", itemRoutes);
+app.use("/user", userRoutes);
+app.use("/cart", cartRoutes);
+app.use("/latest", latestRoutes);
+app.use("/order", orderRoutes);
+app.use("/report", reportRoutes);
 
 // sequelize.sync({ force: true }).then((result) => {
 //     console.log("migration successful");
